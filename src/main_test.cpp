ace2309240b613510bc07f50bfc26fdb43ce15ac
notnuff <113949291+notnuff@users.noreply.github.com>
2024-10-08 22:40:22 +0300

    make test work again

--- src/main_test.cpp
+++ src/main_test.cpp
@@ -3,7 +3,7 @@
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
     // Expect equality.
-    EXPECT_EQ(7 * 1, 8);
+    EXPECT_EQ(7 * 1, 7);
 }// Demonstrate some basic assertions.
 TEST(HelloTest, StringsEq) {
     // Expect two strings not to be equal.
